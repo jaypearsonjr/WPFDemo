c1fe3a1a373206d148fe4f33dea1a7a35e8d2ddb
Jay Pearson <jaypearsonjr@gmail.com>
2018-06-07 01:47:20 -0400

    added gitignore, fixed path issue to asset, added Entity framwork package, added models

---
+++ Model/Expense.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseIt.Model
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+
+        public string ExpenseType { get; set; }
+        public string ExpenseAmount { get; set; }
+
+        public virtual List<Expense> Expenses { get; set; }
+
+    }
+}
---
+++ Model/People.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseIt.Model
+{
+    class People
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Department { get; set; }
+
+        public virtual List<People> Persons { get; set; }
+    }
+}
